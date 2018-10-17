65763c4334188d4338804254fea5378ac128a418
Lucas de Oliveira Silva <lucas.silva@dextra-sw.com>
2018-10-17 14:50:31 -0300

    lab10 doc

--- lab10/arvore.h
+++ lab10/arvore.h
@@ -1,7 +1,7 @@
 #ifndef MC202_ARVORE_H
 #define MC202_ARVORE_H
 
-#define TAM_MAX 12 /* Tamanho do maior int possivel (em caracteres) -2147483648 */
+#define TAM_MAX 12 /* Tamanho do maior int possivel (em caracteres): -2147483648 */
 
 typedef enum TipoDado {
     inteiro, caractere
@@ -9,24 +9,25 @@ typedef enum TipoDado {
 
 typedef union Dado {
     int numero;
-    char operador_ou_variavel;
+    char operador_ou_variavel; /* Pode ser uma letra, +, -, * ou / */
 } Dado;
 
+/* No com uma Tagged Union: */
 typedef struct No {
     Dado dado;
-    TipoDado tipo;
+    TipoDado tipo; /* Indica qual o tipo do dado na Union */
     struct No *esq, *dir;
 } No;
 
-typedef No *expressao;
+typedef No *expressao; /* Arvore de expressao */
 
 expressao cria_expressao(Dado dado, TipoDado tipo, expressao esq, expressao dir);
 
 void imprime_como_exp(expressao exp);
 
-expressao simplifica(expressao exp);
+expressao simplifica(expressao exp); /* Simplifica quando possivel */
 
-expressao le_expressao();
+expressao le_expressao(); /* Essa funcao foi criada aqui para evitar o acesso direto das estruturas */
 
 void destroi_expressao(expressao exp);
 
