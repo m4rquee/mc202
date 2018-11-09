36f1606a958f0f02da7d1dbc7906f3240c603ff9
Lucas de Oliveira Silva <lucas.silva@dextra-sw.com>
2018-11-09 10:12:17 -0300

    lab13 struct

---
+++ lab13/hash.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include "hash.h"
+
+p_autor cria_vetor(size_t n) {
+    p_autor ret = calloc(n, sizeof(Autor));
+    if (ret == NULL) {
+        printf("Erro ao alocar memoria");
+        exit(EXIT_FAILURE);
+    }
+    return ret;
+}
+
+int hash1(char nome[TAM_NOME]) {
+    int i, n = 0;
+    for (i = 0; i < TAM_NOME; i++)
+        n = (256 * n + nome[i]) % MAX;
+
+    return n;
+}
+
+int hash2(char nome[TAM_NOME]) {
+    int i, n = 0;
+    for (i = TAM_NOME - 1; i >= 0; i--)
+        n = (256 * n + nome[i]) % MAX;
+
+    return n | 1; /* Garante que sera impar, ou seja co-primo a MAX */
+}
+
+Hash criar_hash() {
+    Hash ret;
+    ret.vetor = cria_vetor(MAX);
+    return ret;
+}
+
+void insere(Hash hash, char nome[TAM_NOME]) {
+    int cod = hash1(nome), pulo = hash2(nome), i = 0;
+
+    while (i++ != MAX) { /* A condicao evita loop */
+        if (!hash.vetor[cod].nome[0]) /* Checa se a posicao e vazia */
+            strcpy(hash.vetor[cod].nome, nome);
+
+        cod = (cod + pulo) % MAX; /* O vetor e "circular" */
+    }
+}
+
+char contem(Hash hash, char nome[TAM_NOME], int *pos) {
+    int cod = hash1(nome), pulo = hash2(nome), i = 0;
+    *pos = -1; /* Indica que nao existe */
+
+    while (i++ != MAX) { /* A condicao evita loop */
+        if (!hash.vetor[cod].nome[0]) /* Checa se a posicao e vazia */
+            return 0;
+        else if (strcmp(nome, hash.vetor[cod].nome)) { /* Achou o autor */
+            *pos = cod;
+            return 1;
+        }
+
+        cod = (cod + pulo) % MAX; /* O vetor e "circular" */
+    }
+
+    return 0;
+}
+
+void atualiza_conexoes(Hash hash, char nome[TAM_NOME], unsigned long long novas_conexoes) {
+    int pos;
+    if (contem(hash, nome, &pos))
+        hash.vetor[pos].conexoes |= novas_conexoes;
+}
+
+char possui_conexao(Hash hash, char nome[TAM_NOME], unsigned long long conexao) {
+    int pos;
+    /* Como cada bit representa uma conexao, ao fazer o "ou bit a bit" o valor nao se altera caso a conexao exista: */
+    return contem(hash, nome, &pos) && (hash.vetor[pos].conexoes | conexao == hash.vetor[pos].conexoes);
+}
+
+void destroi_hash(Hash hash) {
+    free(hash.vetor);
+}
---
+++ lab13/hash.h
@@ -0,0 +1,34 @@
+#ifndef MC202_HASH_H
+#define MC202_HASH_H
+
+#define MAX 64
+#define TAM_NOME 19 /* Inicial mais sobrenome */
+
+typedef struct Autor {
+    char nome[TAM_NOME];
+    /* Cada bit indica uma colaboracao entre esse autor e o autor no heap na mesma possicao que o bit,
+     * ou seja e uma especie de vetor caracteristico: */
+    unsigned long long conexoes;
+} Autor;
+
+typedef Autor *p_autor;
+
+typedef struct Hash {
+    p_autor vetor;
+} Hash;
+
+Hash criar_hash();
+
+void insere(Hash hash, char nome[TAM_NOME]);
+
+char contem(Hash hash, char nome[TAM_NOME], int *pos);
+
+void atualiza_conexoes(Hash hash, char nome[TAM_NOME], unsigned long long novas_conexoes);
+
+void atualiza_conexoes(Hash hash, char nome[TAM_NOME], unsigned long long novas_conexoes);
+
+char possui_conexao(Hash hash, char nome[TAM_NOME], unsigned long long conexao);
+
+void destroi_hash(Hash hash);
+
+#endif
